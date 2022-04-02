bc38f5a58ea08d5bad3e48d7a950a9f629cd569a
Ciilll <asdf@gmail.com>
2022-04-02 23:30:06 +0700

    SL

--- SL2_AplikasiPengelolaKeuangan - DB/loginProcess.php
+++ SL2_AplikasiPengelolaKeuangan - DB/loginProcess.php
@@ -3,11 +3,13 @@
     include("config.php");
     
     if(isset($_POST['loginSubmit'])){
-        $str_query = "SELECT * FROM user";
+        // echo " <script>
+        //     alert ('$_POST[username]');
+        // </script>";
+        $str_query = "SELECT * FROM user WHERE username = '".$_POST['loginusername']."'";
         $query = mysqli_query($connection, $str_query);
-
-        //search username in database
-        while($data = mysqli_fetch_array($query)){
+        $data = mysqli_fetch_array($query);
+        if($data){
             if($data['username'] == $_POST['loginusername']){
                 if($data['password'] == $_POST['loginpassword']){
                     $_SESSION['username'] = $data['username'];
